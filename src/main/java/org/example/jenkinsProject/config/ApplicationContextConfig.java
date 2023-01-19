21fe3f6e15d6de0429f18acb93c02a3a55779e79
puzan <puzanpuzani@gmail.com>
2023-01-19 12:59:06 +0400

    Add htm files. Workable App

--- src/main/java/org/example/jenkinsProject/config/ApplicationContextConfig.java
+++
@@ -1,2 +0,0 @@
-package org.example.jenkinsProject.config;public class ApplicationContextConfig {
-}
--- src/main/java/org/example/jenkinsProject/controller/HelloController.java
+++ src/main/java/org/example/jenkinsProject/controller/HelloController.java
@@ -1,2 +1,18 @@
-package org.example.jenkinsProject.controller;public class HelloController {
+package org.example.jenkinsProject.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class HelloController {
+    @GetMapping("/hello")
+    public String helloPage(){
+        return "hello";
+    }
+    @GetMapping("/goodbye")
+    public String goodBye(){
+        return "goodbye";
+    }
 }
